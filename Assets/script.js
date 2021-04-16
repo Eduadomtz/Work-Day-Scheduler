24ba4d6c3b3a0eafe18d95b9c0598d98631a711b
Eduardo Martinez <eduardo31794@gmail.com>
2021-04-16 23:08:48 +1000

    Updating save button

--- Assets/script.js
+++ Assets/script.js
@@ -2,15 +2,17 @@ var today = moment();
 $("#currentDay").text(today.format("dddd, MMMM Do"));
 
 function theTime() {
-    var hour = moment().hour();
+
 
     $(".time-block").each(function () {
-        var time = parseInt($(this).attr("id"));
+        var current = moment().hours();
 
-        if (hour > time) {
+        var time = parseInt($(this).attr("id").split("hours")[1]);
+
+        if (current > time) {
             $(this).addClass("past");
         }
-        else if (hour === time) {
+        else if (current === time) {
             $(this).removeClass("past");
             $(this).addClass("present");
         }
@@ -21,6 +23,33 @@ function theTime() {
         }
     })
 }
-$(document).ready( function() {
-    theTime();
-});
+
+theTime();
+
+
+
+/*
+
+
+$("#hour9 .description").val(localStorage.getItem("hour9"));
+$("#hour10 .description").val(localStorage.getItem("hour10"));
+$("#hour11 .description").val(localStorage.getItem("hour11"));
+$("#hour12 .description").val(localStorage.getItem("hour12"));
+$("#hour13 .description").val(localStorage.getItem("hour13"));
+$("#hour14 .description").val(localStorage.getItem("hour14"));
+$("#hour15 .description").val(localStorage.getItem("hour15"));
+$("#hour16 .description").val(localStorage.getItem("hour16"));
+$("#hour17 .description").val(localStorage.getItem("hour17"));
+
+
+
+$("#hour9").attr("data-time", moment("9:00 am", "h:mm a").format("HH"));
+$("#hour10").attr("data-time", moment("10:00 am", "hh:mm a").format("HH"));
+$("#hour11").attr("data-time", moment("11:00 am", "hh:mm a").format("HH"));
+$("#hour12").attr("data-time", moment("12:00 pm", "hh:mm a").format("HH"));
+$("#hour13").attr("data-time", moment("1:00 pm", "h:mm a").format("HH"));
+$("#hour14").attr("data-time", moment("2:00 pm", "h:mm a").format("HH"));
+$("#hour15").attr("data-time", moment("3:00 pm", "h:mm a").format("HH"));
+$("#hour16").attr("data-time", moment("4:00 pm", "h:mm a").format("HH"));
+$("#hour17").attr("data-time", moment("5:00 pm", "h:mm a").format("HH"));
+*/
